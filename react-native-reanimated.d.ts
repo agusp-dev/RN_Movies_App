864492fcbea0f1b1675092e631882ebeff522187
Agustin <perezp.agustin@gmail.com>
2025-07-06 20:42:03 -0300

    add new libs config - add buttons and texts to home screen

---
+++ react-native-reanimated.d.ts
@@ -0,0 +1,8 @@
+// react-native-reanimated.d.ts
+import 'react-native-reanimated';
+
+declare module 'react-native-reanimated' {
+  interface AnimatedProps {
+    // Reanimated 3+
+  }
+}
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,8 +1,16 @@
-import { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { useEffect, useRef } from 'react';
+import { View, Text, Dimensions, TouchableOpacity, StyleSheet } from 'react-native';
 import { getPopularMovies } from '../../utils'
+import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel';
+import { useSharedValue } from 'react-native-reanimated';
 
 export const HomeScreen = () => {
+
+  const data = [...new Array(6).keys()];
+  const width = Dimensions.get("window").width;
+
+  const ref = useRef<ICarouselInstance>(null);
+  const progress = useSharedValue<number>(0);
 
   useEffect(() => {
     getPopularMovies().then(data => {
@@ -12,7 +20,104 @@ export const HomeScreen = () => {
 
   return (
     <View>
-      <Text>Home Screen</Text>
+      <View>
+        <Text>My List</Text>
+        <Text>Discover</Text>
+      </View>
+      <Carousel
+        ref={ref}
+        width={width}
+        height={width / 2}
+        data={data}
+        onProgressChange={progress}
+        renderItem={({ index }) => (
+          <View
+            style={{
+              flex: 1,
+              borderWidth: 1,
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ textAlign: "center", fontSize: 30 }}>{index}</Text>
+          </View>
+        )}
+      />
+      <View style={styles.buttonSection}>
+        <TouchableOpacity style={styles.buttonWishList}>
+          <Text style={styles.textWishList}>+ WishList</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonDetail}
+        >
+          <Text style={styles.textDetails}>Details</Text>
+        </TouchableOpacity>
+    </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    scrollViewContainer: {
+        backgroundColor: '#000000',
+    },
+    carouselContainer: {
+        flex: 1,
+        backgroundColor: '#000000',
+    },
+    carouselSection: {
+        position: 'relative',
+    },
+    overlayContainer: {
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        paddingBottom: 5,
+    },
+    titleSection: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 20,
+    },
+    buttonSection: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 20,
+    },
+    textDesc: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        paddingVertical: 20,
+        color: 'white',
+    },
+    buttonDetail: {
+        backgroundColor: '#F2C94C',
+        paddingHorizontal: 50,
+        paddingVertical: 20,
+        borderRadius: 13,
+        alignItems: 'center',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    textWishList: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    buttonWishList: {
+        backgroundColor: '#333333',
+        paddingHorizontal: 50,
+        paddingVertical: 20,
+        borderRadius: 13,
+        alignItems: 'center',
+    },
+    textDetails: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+});
